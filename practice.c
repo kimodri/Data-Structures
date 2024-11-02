718dd82799503269afc8fa7451a12c34fb181d87
kimodri <work.kimmagan@gmail.com>
2024-11-02 12:24:01 +0800

    Added a while loop that prints 0 to 5
    Try niyo nga mga pre, gumawa rin ng branch ng sarili niyo

--- practice.c
+++ practice.c
@@ -5,4 +5,12 @@ int main()
     printf("This is a C file to practice collaboration.\n");
     printf("1. Try creating a branch from the main.\n");
     printf("2. Make a pull request\n");
+
+    // Added from kim branch
+    int x = 0;
+    while(x <= 5)
+    {
+        printf("%d\n", x);
+        x++;
+    }
 }
